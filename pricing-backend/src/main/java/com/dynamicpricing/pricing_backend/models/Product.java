271b948816b37c2502391e519ded0c0b7bdff2e0
Abhijit007435 <hunterajm123@gmail.com>
2026-07-05 16:54:46 +0530

    Setup Product model and MongoDB integration

---
+++ pricing-backend/src/main/java/com/dynamicpricing/pricing_backend/models/Product.java
@@ -0,0 +1,26 @@
+package com.dynamicpricing.pricing_backend.models;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "products")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
+    @Id
+    private String id;
+
+    private String productName;
+
+    private String category;
+
+    private Double currentPrice;
+
+    private LocalDateTime createdAt;
+}
