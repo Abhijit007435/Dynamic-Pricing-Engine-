7787893c92d3068299cf19963b51fb2997d031b6
Abhijit007435 <hunterajm123@gmail.com>
2026-07-05 14:57:59 +0530

    backend initialized

---
+++ pricing-backend/src/main/java/com/dynamicpricing/pricing_backend/PricingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dynamicpricing.pricing_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingBackendApplication.class, args);
+	}
+
+}
