3e569f00aa231e5455d0bd7883b2d1886d34d8e3
Abhijit007435 <hunterajm123@gmail.com>
2026-07-07 05:04:37 +0530

    Add global exception handling

---
+++ pricing-backend/src/main/java/com/dynamicpricing/pricing_backend/dtos/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.dynamicpricing.pricing_backend.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String message;
+    private int status;
+    private LocalDateTime timestamp;
+}
